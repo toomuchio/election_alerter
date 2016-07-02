0a0d48625d7ba6800705c783b62b3d809bb8d67e
Too Much IO <toomuchio@users.noreply.github.com>
2016-07-03 04:32:47 +1000

    Create alert.py
    Hacked together to keep me updated during this years election cycle

---
+++ alert.py
@@ -0,0 +1,62 @@
+import urllib2, time, smtplib
+from bs4 import BeautifulSoup
+from prettytable import PrettyTable
+
+SETTINGS = {}
+SETTINGS["GMAIL_USERNAME"] = "changeme"
+SETTINGS["GMAIL_PASSWORD"] = "changeme"
+SETTINGS["ALERT_EMAILS"] = ["changeme", "changeme2"]
+SETTINGS["REFRESH_RATE"] = 60
+
+def send_email(username, password, recipients, subject, body):
+	try:
+		gmail = smtplib.SMTP("smtp.gmail.com", 587)
+		gmail.ehlo()
+		gmail.starttls()
+		gmail.login(username, password)
+		gmail.sendmail(username, recipients, "From: " + username + "\nTo: " + ", ".join(recipients) + "\nSubject: " + subject + "\n\n" + body)
+		gmail.close()
+		return True
+	except:
+		return False
+
+def eStatus():
+	results = []
+
+	try:
+		aRequest = urllib2.Request("http://vtr.aec.gov.au/HouseDefault-20499.htm", headers={'User-Agent': 'Mozilla/5.0'})
+		aPage = urllib2.urlopen(aRequest).read()
+		aSoup = BeautifulSoup(aPage)
+	except:
+		return results
+
+	answers = aSoup.find("table", { "id" : "datatable" })
+	if not answers:
+		return results
+
+	rows = answers.find("tbody").find_all("tr")
+	if not rows or len(rows) == 0:
+		return results
+
+	for row in rows:
+		tmp = row.findAll("td")
+		results.append([row.find("th").get_text(), tmp[0].get_text(), tmp[1].get_text()])
+
+	results.sort(key=lambda results: results[1])
+	return results
+
+last = None
+current = None
+while 1:
+	last = current
+	current = eStatus()
+	if last != current:
+		table_results = PrettyTable()
+		table_results.field_names =["Party", "Seats", "Close"]
+		for row in reversed(current):
+			table_results.add_row([row[0], row[1], row[2]])
+		tmp = "\n\nResults Updated @ " + time.ctime() + "\n" + table_results.get_string()
+		send_email(SETTINGS['GMAIL_USERNAME'], SETTINGS['GMAIL_PASSWORD'], SETTINGS['ALERT_EMAILS'], "Polling Updates!", tmp)
+		print tmp
+	time.sleep(SETTINGS['REFRESH_RATE'])
+
